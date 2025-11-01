6cfb4c8fc365f03d858ca762b7208428f106a82d
jakejacobs2015 <jacky4uonly2009@gmail.com>
2025-11-01 16:21:16 +0530

    Intial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+# print(1 + 1)
+
+# print('hi ' + 'there')
+
+# print('bye')
+
+# print('holiday ' * 15)
+
+# print('My age will be ' + str(28 + 1.5) + ' in one year.')
+
+# street = 1
+# speed = 4.6
+# firstname = 'jacobs'
+
+# greet = 'hi'
+# age = 29
+
+# print('My age will be ' + str(age + 1.5) + ' in one year.')
+
+# print(age)
+
+# print(greet)
+
+
+# members = 35
+# expected_grade = 8.5
+# least_grade = 5
+# print('Good morning!')
+# print('Hello how are you?')
+# print('Welcome to my class!')
+# print('There should be ' + str(members) + ' members in the class.')
+# print('The final grade must be greater than ' + str(expected_grade) + ' out of 10')
+# print('I will give ' + str(least_grade + 2) + ' points for good attendance.')
+
+
+# first_name = 'jake'
+# print(first_name)
+
+# age = 23
+
+# print(age)
+
+
+student_marks = 55
+expected_marks = 90
+
+if student_marks > expected_marks:
+    print('laptop')
+    print('iphone')
+    print('earphones')
+    print('bike')
+else:
+    print('wash dishes')
+    print('clean floor')
+    print('no playing video games')
